8c7d11a03e0d8ecb31dba989f5c3d2552cf88a9b
Thomas Szteliga <ts@websafe.pl>
2013-11-08 01:45:07 +0100

    Updated config.

--- config/module.config.php
+++ config/module.config.php
@@ -4,28 +4,11 @@ namespace WebsafeZfModZfcUserI18nPlPl;
 
 return array(
     'translator'      => array(
-/*
-        'translation_file_patterns' => array(
-            array(
-                'type'     => 'gettext',
-                'base_dir' => __DIR__ . '/../language',
-                'pattern'  => '%s.mo',
-            ),
-            array(
-                'type'     => 'gettext',
-                'base_dir' => __DIR__ . '/../language',
-                'pattern'  => '%s.mo',
-            ),
-        ),
-    ),
-*/
         'translation_files' => array(
             array(
                 'type'     => 'gettext',
-                'base_dir' => __DIR__ . '/../language',
-                'pattern'  => 'pl_PL.mo',
+                'filename'  => __DIR__ . '/../language/pl_PL.mo',
             ),
         ),
     ),
-
 );
