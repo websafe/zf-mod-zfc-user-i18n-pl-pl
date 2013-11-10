9baa3b29ba8c338fe7a2dd1532705ef80a1b0f65
Thomas Szteliga <ts@websafe.pl>
2013-11-10 03:03:02 +0100

    Added locale to translation file in config.

--- config/module.config.php
+++ config/module.config.php
@@ -3,12 +3,12 @@
 namespace WebsafeZfModZfcUserI18nPlPl;
 
 return array(
-    'translator'      => array(
+    'translator' => array(
         'translation_files' => array(
             array(
-                'type'      => 'gettext',
-                'filename'  => __DIR__ . '/../language/pl_PL.mo',
-                'locale'    => 'pl_PL'
+                'type'     => 'gettext',
+                'filename' => __DIR__ . '/../language/pl_PL.mo',
+                'locale'   => 'pl_PL'
             ),
         ),
     ),
