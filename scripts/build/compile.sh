3772a368721b6bad5c303402f442e21e8704cd33
Thomas Szteliga <ts@websafe.pl>
2013-11-10 04:28:19 +0100

    Small update.

--- scripts/build/compile.sh
+++ scripts/build/compile.sh
@@ -4,6 +4,7 @@
 SCRIPT_DIR=$(dirname ${0})
 PROJECT_DIR=${SCRIPT_DIR}/../..
 LANG_DIR=${PROJECT_DIR}/language/
+LOCALE=pl_PL
 
 #
-msgfmt -cv -o ${LANG_DIR}/pl_PL.mo ${LANG_DIR}/pl_PL.po
+msgfmt -cv -o ${LANG_DIR}/${LOCALE}.mo ${LANG_DIR}/${LOCALE}.po
